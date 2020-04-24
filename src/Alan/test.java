5a4fb715f75344428f685ab4fe207ce17b8bbc06
羊习习嗷 <1224177203@qq.com>
2020-04-25 02:27:30 +0800

    50% 存档

--- src/Alan/test.java
+++ src/Alan/test.java
@@ -6,12 +6,6 @@ public class test {
 
     public static void main(String[] args) {
 
-        System.out.println("请输入一个秒数");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int s = a / 3600;
-        int f = a % 3600 / 60;
-        int m = a % 60;
-        System.out.println(s + "时" + f + "分" + m + "秒");
+        System.out.println((11+3*8)/4%7);
     }
 }
