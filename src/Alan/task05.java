63471e6c051369ec207b76aa9551413e02ac1f20
羊习习嗷 <1224177203@qq.com>
2020-04-25 08:35:29 +0800

    修正

--- src/Alan/task05.java
+++ src/Alan/task05.java
@@ -6,22 +6,22 @@ public class task05 {
 
     public static void main(String[] args) {
 
-        String[][] num = new String[16][16];
+        String[][] num = new String[17][17];
 
-        for (int i = 0; i < 16; i++) {
+        for (int i = 0; i < 17; i++) {
 
-            for (int j = 0; j < 16; j++) {
+            for (int j = 0; j < 17; j++) {
 
                 if (i == 0 && j == 0) {
                     num[i][j] = " ";
-                } else if (i == 0 && j > 9) {
-                    num[i][j] = ((char) (j + 87)) + ""; // 转化ASCII码中的小写字母
+                } else if (i == 0 && j > 10) {
+                    num[i][j] = ((char) (j - 1 + 87)) + ""; // 转化ASCII码中的小写字母
                 } else if (i == 0) {
-                    num[i][j] = j + "";
-                } else if (j == 0 && i > 9) {
-                    num[i][j] = ((char) (i + 87)) + "";
+                    num[i][j] = j - 1 + "";
+                } else if (j == 0 && i > 10) {
+                    num[i][j] = ((char) (i - 1 + 87)) + "";
                 } else if (j == 0) {
-                    num[i][j] = i + "";
+                    num[i][j] = i - 1 + "";
                 } else {
                     num[i][j] = "+";
                 }
@@ -29,9 +29,9 @@ public class task05 {
             }
         }
 
-        for (int i = 0; i < 16; i++) {
+        for (int i = 0; i < 17; i++) {
 
-            for (int j = 0; j < 16; j++) {
+            for (int j = 0; j < 17; j++) {
 
                 System.out.print(num[i][j] + "\t");
             }
