597b4441c551495aa7fb7e487bb4d5b1ff3670a7
羊习习嗷 <1224177203@qq.com>
2020-05-07 12:24:24 +0800

    阶段1模块2 36%

--- src/Alan/task01.java
+++ src/Alan/task01.java
@@ -1,4 +1,9 @@
 package Alan;
 
 public class task01 {
+    public static void main(String[] args) {
+        int[] a = new int[]{100};
+        a = new int[]{10, 10};
+        System.out.println(a[0]);
+    }
 }
